867a5bee8429761406414181662ec48ccf892573
Dwaipayan Ray <dwaipayanray1@gmail.com>
2020-10-16 02:19:06 +0530

    Dev: Refactor code and add examples

---
+++ examples/cpp-example.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+
+	"github.com/raydwaipayan/test-runner/runner/run"
+)
+
+func main() {
+
+	dat, _ := ioutil.ReadFile("a.cpp")
+	in := []string{"2", "4", "5"}
+	out := []string{"4", "16", "25"}
+
+	res := run.Evaluate(string(dat), "cpp", "a.cpp", in, out, 3, 1, 500*1024*1024)
+	log.Print(res)
+}
