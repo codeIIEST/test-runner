87ccbb2bafc2ade77ce2b508560393adac3515ab
Anik Das <anikdas0811@gmail.com>
2020-11-14 22:30:04 +0530

    - upd: requested changes

--- test/languages/c/c-example.go
+++ test/languages/c/c-example.go
@@ -5,16 +5,14 @@ import (
 	"log"
 
 	"github.com/codeiiest/test-runner/runner/run"
-	util "github.com/codeiiest/test-runner/test/utils"
 )
 
 // Test is used to test C
 func Test() {
-
 	dat, _ := ioutil.ReadFile("samples/c/a.c")
 	in := []string{""}
 	out := []string{"20"}
-	util.GetFilesInPwd()
+
 	res := run.Evaluate(string(dat), "c", "a.c", in, out, 1, 1, 500*1024*1024)
 	log.Print(res)
 }
--- test/languages/cpp/cpp-example.go
+++ test/languages/cpp/cpp-example.go
@@ -5,16 +5,14 @@ import (
 	"log"
 
 	"github.com/codeiiest/test-runner/runner/run"
-	util "github.com/codeiiest/test-runner/test/utils"
 )
 
 // Test is used to test CPP
 func Test() {
-
 	dat, _ := ioutil.ReadFile("samples/cpp/a.cpp")
 	in := []string{"2", "4", "5"}
 	out := []string{"4", "16", "25"}
-	util.GetFilesInPwd()
+
 	res := run.Evaluate(string(dat), "cpp", "a.cpp", in, out, 3, 1, 500*1024*1024)
 	log.Print(res)
 }
